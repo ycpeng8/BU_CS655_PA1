cafb53d69708e983ac19158a4186bb586c2d43a5
Jinghao Ye <jy3767a@student.american.edu>
2021-09-27 14:02:27 -0400

    socket test

---
+++ part2Test/Client.java
@@ -0,0 +1,70 @@
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+    public static String wp =" ";
+    public static void Send_CSP(){
+
+    }
+    public static void Send_MP(String PROBE_SEQUENCE_NUMBER, String MESSAGE_SIZE, PrintStream ps){
+	    String ws=" ";
+	    String MP_MSG = "s"+ws+PROBE_SEQUENCE_NUMBER+ws+MESSAGE_SIZE+ws+"\\n";
+	}
+    public static void main(String[] args) throws IOException
+    {
+        // Get hostname and portnumber from command line
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try
+        {
+            // Establish socket
+            Socket socket = new Socket(hostName, portNumber);
+            System.out.println("---------- Connect successfully with the server "+ hostName + ": " + portNumber + " ----------");
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+            byte[] buf = new byte[1024];
+            int readLen = 0;
+//            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("input csp message");
+            BufferedReader cspInput = new BufferedReader(new InputStreamReader(System.in));
+            String cspmsg = cspInput.readLine();
+            out.write(cspmsg.getBytes());
+            socket.shutdownOutput();
+
+            System.out.println("second msg");
+            BufferedReader r2 = new BufferedReader(new InputStreamReader(System.in));
+            String m2 = r2.readLine();
+            out.write(m2.getBytes());
+//            System.out.println("client send new msg "+cspmsg);
+//            out.println(cspmsg);
+//            String SReturnCSPMSG = in.readLine();
+//            System.out.println("The server sends back the message: " + SReturnCSPMSG);
+//            System.out.println("output byte array");
+//            if(SReturnCSPMSG.equals("200 OK: Ready")){
+//                String[] CSPParams = cspmsg.split("\\s+");
+//                String PROBE_SEQUENCE_NUMBER = CSPParams[2];
+//                String MESSAGE_SIZE = CSPParams[3];
+//                String MPMSG = "m"+wp+PROBE_SEQUENCE_NUMBER+wp+MESSAGE_SIZE;
+//                System.out.println("client send mpmsg "+MPMSG);
+////                out.println(MPMSG);
+//            }
+
+            // Shutdown stream
+//            stdIn.close();
+            cspInput.close();
+            in.close();
+            out.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            System.out.println("---------- Client closes ----------");
+        }
+    }
+}
---
+++ part2Test/MultiThread.java
@@ -0,0 +1,92 @@
+import java.net.*;
+import java.io.*;
+
+public class MultiThread extends Thread
+{
+    private Socket socket = null;
+    private String CSPreturnMsg;
+
+    private boolean termination = false;
+
+    public MultiThread(Socket socket)
+    {
+        super("MultiThread");
+        this.socket = socket;
+    }
+    public void MPMSGCheck(String  MPmsg){
+
+    }
+    public void CSPMsgCheck(String cspmsg){
+        String[] CSPValidation = cspmsg.split("\\s+");
+        if(!CSPValidation[CSPValidation.length-1].equals("\\n") ||
+                CSPValidation.length != 6 ||
+                !CSPValidation[0].equals("s")||
+                (!CSPValidation[1].equals("rtt") && !CSPValidation[1].equals("tput"))||
+                !isInt(CSPValidation[2])||
+                !isInt(CSPValidation[3])||
+                !isInt(CSPValidation[4])
+        ){
+            CSPreturnMsg = "404 Error";
+            termination = true;
+        }else{
+            CSPreturnMsg = "200 OK: Ready";
+        }
+    }
+
+    public boolean isInt(String s){
+        try{
+            Integer.parseInt(s);
+        }catch (NumberFormatException e){
+            return false;
+        }catch(NullPointerException e){
+            return false;
+        }
+        return true;
+    }
+
+    public void run()
+    {
+        try
+        {
+//            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+            byte[] buf = new byte[1024];
+            int readLen = 0;
+            String inputCSPMSG =" ";
+            while((readLen = in.read(buf)) != -1){
+                inputCSPMSG = new String(buf,0,readLen);
+            }
+//            System.out.println(inputCSPMSG);
+//            String inputLine, outputLine;
+//            inputLine = in.readLine();
+
+//            while((inputLine = in.readLine()) != null)
+//            {
+////                System.out.println("Receive a message from the client: " + inputLine);
+////                outputLine = inputLine;
+////                out.println(outputLine);
+////                System.out.println("---------- The same message is sent back ----------");
+//                System.out.println("----Client sents "+inputLine+" ----");
+//                CSPMsgCheck(inputLine);
+//                out.println(CSPreturnMsg);
+//                if(!termination){
+//                    inputLine = in.readLine();
+//                    System.out.println(inputLine);
+//                }
+//            }
+            in.close();
+            out.close();
+            socket.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            System.out.println("---------- One client closes ----------");
+        }
+    }
+}
